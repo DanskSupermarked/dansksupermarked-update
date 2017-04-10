5c9e98651ce54fe75c3acd0890df5594b1104ae3
Benjamin Merot <bem.its@dsg.dk>
2017-04-10 13:53:57 +0200

    Correctly split Rake spec test of resources name and path

--- spec/classes/update_spec.rb
+++ spec/classes/update_spec.rb
@@ -5,20 +5,20 @@ describe 'update' do
   context 'without Python lib deps for RedHat 6' do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '6', :operatingsystemrelease => '6.9'} }
     let(:params) { { 'manage_python_deps' => false } }
-    it { should contain_file('/usr/local/bin/send_pull_request.py') }
-    it { should contain_file('/usr/local/bin/generate_list.py') }
-    it { should contain_file('/usr/local/bin/update_context.py') }
-    it { should contain_file('/etc/update-with-puppet.conf') }
+    it { should contain_file('send_pull_request.py').with_path('/usr/local/bin/send_pull_request.py') }
+    it { should contain_file('generate_list.py').with_path('/usr/local/bin/generate_list.py') }
+    it { should contain_file('update_context.py').with_path('/usr/local/bin/update_context.py') }
+    it { should contain_file('update-with-puppet.conf').with_path('/etc/update-with-puppet.conf') }
     it { should contain_package('git') }
   end
 
   context 'without Python lib deps for RedHat 7' do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '7', :operatingsystemrelease => '7.3'} }
     let(:params) { { 'manage_python_deps' => false } }
-    it { should contain_file('/usr/local/bin/send_pull_request.py') }
-    it { should contain_file('/usr/local/bin/generate_list.py') }
-    it { should contain_file('/usr/local/bin/update_context.py') }
-    it { should contain_file('/etc/update-with-puppet.conf') }
+    it { should contain_file('send_pull_request.py').with_path('/usr/local/bin/send_pull_request.py') }
+    it { should contain_file('generate_list.py').with_path('/usr/local/bin/generate_list.py') }
+    it { should contain_file('update_context.py').with_path('/usr/local/bin/update_context.py') }
+    it { should contain_file('update-with-puppet.conf').with_path('/etc/update-with-puppet.conf') }
     it { should contain_package('git') }
   end
 
