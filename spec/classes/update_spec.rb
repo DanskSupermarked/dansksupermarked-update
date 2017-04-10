2e632c425cacaf7deed4714107324e6b75b25f60
Benjamin Merot <bem.its@dsg.dk>
2017-04-10 13:40:23 +0200

    Added operatingsystemrelease Fact assignment to the spec test

--- spec/classes/update_spec.rb
+++ spec/classes/update_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'update' do
 
   context 'without Python lib deps for RedHat 6' do
-    let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '6'} }
+    let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '6', :operatingsystemrelease => '6.9'} }
     let(:params) { { 'manage_python_deps' => false } }
     it { should contain_file('/usr/local/bin/send_pull_request.py') }
     it { should contain_file('/usr/local/bin/generate_list.py') }
@@ -13,7 +13,7 @@ describe 'update' do
   end
 
   context 'without Python lib deps for RedHat 7' do
-    let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '7'} }
+    let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '7', :operatingsystemrelease => '7.3'} }
     let(:params) { { 'manage_python_deps' => false } }
     it { should contain_file('/usr/local/bin/send_pull_request.py') }
     it { should contain_file('/usr/local/bin/generate_list.py') }
