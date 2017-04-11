046d57090eca4060f29b25119664ae68a569d8fd
Benjamin Merot <bem.its@dsg.dk>
2017-04-11 09:10:05 +0200

    Added rspec check for CRON job

--- spec/classes/update_spec.rb
+++ spec/classes/update_spec.rb
@@ -5,6 +5,7 @@ describe 'update' do
   context 'without Python lib deps for RedHat 6' do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '6', :operatingsystemrelease => '6.9'} }
     let(:params) { { 'manage_python_deps' => false } }
+    it { should contain_cron('pkg_update_check').with_command('/usr/local/bin/update_context.py -c /etc/update-with-puppet.conf') }
     it { should contain_file('send_pull_request.py').with_path('/usr/local/bin/send_pull_request.py') }
     it { should contain_file('generate_list.py').with_path('/usr/local/bin/generate_list.py') }
     it { should contain_file('update_context.py').with_path('/usr/local/bin/update_context.py') }
@@ -16,6 +17,7 @@ describe 'update' do
   context 'without Python lib deps for RedHat 7' do
     let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => '7', :operatingsystemrelease => '7.3'} }
     let(:params) { { 'manage_python_deps' => false } }
+    it { should contain_cron('pkg_update_check').with_command('/usr/local/bin/update_context.py -c /etc/update-with-puppet.conf') }
     it { should contain_file('send_pull_request.py').with_path('/usr/local/bin/send_pull_request.py') }
     it { should contain_file('generate_list.py').with_path('/usr/local/bin/generate_list.py') }
     it { should contain_file('update_context.py').with_path('/usr/local/bin/update_context.py') }
